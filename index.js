82e504742fcd3b6b745b27e444cbb96d7a6f523c
Will <willkim0608@gmail.com>
2022-12-08 12:26:48 -0800

    Add writeToFile function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const { title } = require('process');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -13,82 +14,86 @@ const questions = [
     'Choose a license.'
 ];
 
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
+// TODO: Create a function to write README file
+const writeToFile = ({title, description, installation, usage, contributing, tests, license}) =>
+    `# ${title}
 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'title',
-            message: questions[0],
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: questions[1],
-        },
-        {
-            type: 'input',
-            name: 'installation',
-            message: questions[2],
-        },
-        {
-            type: 'input',
-            name: 'usage',
-            message: questions[3],
-        },
-        {
-            type: 'input',
-            name: 'contributing',
-            message: questions[4],
-        },
-        {
-            type: 'input',
-            name: 'tests',
-            message: questions[5],
-        },
-        {
-            type: 'list',
-            name: 'license',
-            message: questions[6],
-            choices: ['License 1', 'License 2', 'License 3', 'License 4', 'License 5']
-        },
-    ])
-  .then((answers) => {
-    const readmeContent = generateReadme(answers);
+## Description
 
-    fs.writeFile('README.md', readmeContent, (err) =>
-      err ? console.log(err) : console.log('Successfully created README.md!')
-    );
-  });
+${description}
 
+## Installation
 
-// const readline = require('readline');
-// const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// });
+${installation}
 
-// for (let i = 0; i < questions.length; i++) {
-//     rl.question('What is your name ? ', function (name) {
-//         rl.question('Where do you live ? ', function (country) {
-//             console.log(`${name}, is a citizen of ${country}`);
-//             rl.close();
-//         });
-//     });
-// }
+## Usage
 
-// rl.on('close', function () {
-//     console.log('\nBYE BYE !!!');
-//     process.exit(0);
-// });
+${usage}
 
-// // // TODO: Create a function to initialize app
-// // function init() {}
+## Contributing
 
-// // // Function call to initialize app
-// // init();
+${contributing}
+
+## Tests
+
+${tests}
+
+## License
+
+${license}`;
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: questions[0],
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: questions[1],
+            },
+            {
+                type: 'input',
+                name: 'installation',
+                message: questions[2],
+            },
+            {
+                type: 'input',
+                name: 'usage',
+                message: questions[3],
+            },
+            {
+                type: 'input',
+                name: 'contributing',
+                message: questions[4],
+            },
+            {
+                type: 'input',
+                name: 'tests',
+                message: questions[5],
+            },
+            {
+                type: 'list',
+                name: 'license',
+                message: questions[6],
+                choices: ['License 1', 'License 2', 'License 3', 'License 4', 'License 5']
+            },
+        ])
+        .then((answers) => {
+            const readmeContent = writeToFile(answers);
+
+            fs.writeFile('README.md', readmeContent, (err) =>
+                err ? console.log(err) : console.log('Successfully created README.md!')
+            );
+        });
+}
+
+// Function call to initialize app
+init();
 
 
 
