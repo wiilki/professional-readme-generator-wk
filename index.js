ad5c3cfd80a2c862f2eb1de7319acc1832599c83
Will <willkim0608@gmail.com>
2022-12-08 10:21:48 -0800

    Add package.json. Add inquirer modules. Add array of questions. Add prompt for answers

--- index.js
+++ index.js
@@ -1,13 +1,64 @@
-// TODO: Include packages needed for this application
+// // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    'What is the title of your project?',
+    'Enter a description of your project.',
+    'How do you install the project?',
+    'How do you use your project?',
+    'How can someone contribute to your project?',
+    'Include any tests for your project.',
+    'Choose a license.'
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: questions[0],
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: questions[1],
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: questions[2],
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: questions[3],
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: questions[4],
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: questions[5],
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: questions[6],
+            choices: ['License 1', 'License 2', 'License 3', 'License 4', 'License 5']
+        },
+    ])
+  .then((answers) => {
+    const readmeContent = generateReadme(answers);
 
-// Function call to initialize app
-init();
+    fs.writeFile('README.md', readmeContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created README.md!')
+    );
+  });
