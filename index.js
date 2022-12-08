27512805468335fe5d4eebab0f577ecedd86986b
Will <willkim0608@gmail.com>
2022-12-08 10:26:44 -0800

    Delete and re-add package.json and inquirer modules

--- index.js
+++ index.js
@@ -62,3 +62,33 @@ inquirer
       err ? console.log(err) : console.log('Successfully created README.md!')
     );
   });
+
+
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// for (let i = 0; i < questions.length; i++) {
+//     rl.question('What is your name ? ', function (name) {
+//         rl.question('Where do you live ? ', function (country) {
+//             console.log(`${name}, is a citizen of ${country}`);
+//             rl.close();
+//         });
+//     });
+// }
+
+// rl.on('close', function () {
+//     console.log('\nBYE BYE !!!');
+//     process.exit(0);
+// });
+
+// // // TODO: Create a function to initialize app
+// // function init() {}
+
+// // // Function call to initialize app
+// // init();
+
+
+
