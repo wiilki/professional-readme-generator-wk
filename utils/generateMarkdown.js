e963abf768d04dba396424ec2351271f3b82c389
Will <willkim0608@gmail.com>
2022-12-12 14:16:35 -0800

    Add 'if no license' value to renderLicenseLink

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,24 +2,31 @@ const fs = require('fs');
 
 // Returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+
+}
 
 // Returns the license link
+// If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (license === 'Apache 2.0') {
-    return `https://www.apache.org/licenses/LICENSE-2.0.txt`
-  }
-  if (license === 'GNU GPLv3') {
-    return `https://www.gnu.org/licenses/gpl-3.0.txt`
-  }
-  if (license === 'MIT') {
-    return `https://www.mit.edu/~amini/LICENSE.md` 
-  }
+  if (!license) {
+    return ``;
+  } else {
+    if (license === 'Apache 2.0') {
+      return `https://www.apache.org/licenses/LICENSE-2.0.txt`;
+    };
+    if (license === 'GNU GPLv3') {
+      return `https://www.gnu.org/licenses/gpl-3.0.txt`;
+    };
+    if (license === 'MIT') {
+      return `https://www.mit.edu/~amini/LICENSE.md`;
+    };
+  };
 }
 
 // Returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
